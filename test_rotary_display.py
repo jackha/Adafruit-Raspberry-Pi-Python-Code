f9635b09dc40581e68433efdd420e26d5e8fb612
Jack Ha <jackha@gmail.com>
2013-10-27 15:08:35 +0000

    test

--- test_rotary_display.py
+++ test_rotary_display.py
@@ -12,24 +12,31 @@ segment = SevenSegment(address=0x74)
 
 value = 0
 
+print 'Test display and rotary encoder'
+
 while(True):
 
     # read rotary encoder
-    value += encoder.get_delta()
+    delta = encoder.get_delta()
+
+    if delta != 0:
+	value += delta
+        print 'change value: %s delta %d' % (value, delta) 
+    	
 
     # Set 7 segment
     # Set hours
-    segment.writeDigit(0, int(value/10000)%10000)
-    segment.writeDigit(1, int(value/1000)%1000) 
+    #segment.writeDigit(0, int(value/10000)%10000)
+    #segment.writeDigit(1, int(value/1000)%1000) 
     # Set minutes
     segment.writeDigit(3, int(value / 100) % 100)   # Tens
     segment.writeDigit(4, value % 10)        # Ones
     # Toggle color
-    segment.setColon(0)              # Toggle colon at 1Hz
+    #segment.setColon(0)              # Toggle colon at 1Hz
 
-    for x in range(0, 8):
-        for y in range(0, 8):
-            color = 1 if (y*8+x) < value else 0
-            grid.setPixel(x, y, color)
+    #for x in range(0, 8):
+    #    for y in range(0, 8):
+    #        color = 1 if (y*8+x) < value else 0
+    #        grid.setPixel(x, y, color)
 
     # sleep(0.001)
