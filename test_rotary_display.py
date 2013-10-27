a421aaecaef0905f14bd4c468c879f10620b01ce
Jack Ha <jackha@gmail.com>
2013-10-27 17:05:44 +0100

    fix

--- test_rotary_display.py
+++ test_rotary_display.py
@@ -27,11 +27,13 @@ class EightByEightPlus(EightByEight):
 
     def set_values(self, values, selected=0):
         lookup_add = [2, 4, 8, 16, 32, 64, 128]
+        print 'display values'
         for row in range(0, 8):
             row_value = 1 if row == selected else 0
             for col in range(0, 7):
                 if values[row] < col * 10:
                     row_value += lookup_add[col]
+            print 'row, rowvalue %d %d' % (row, row_value)
             grid.writeRowRaw(row, row_value, update=False)
         grid.disp.writeDisplay()
 
