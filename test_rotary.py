089a109a4d47d19135bdb7e7bfc990aca067aefd
Jack Ha <jackha@gmail.com>
2013-10-27 15:22:21 +0100

    rotary

--- test_rotary.py
+++ test_rotary.py
@@ -1,10 +1,10 @@
 import rotary_encoder
 
-A_PIN = 7  # use wiring pin numbers here
-B_PIN = 9
+A_PIN = 0  # use wiring pin numbers here
+B_PIN = 2
 encoder = rotary_encoder.RotaryEncoder(A_PIN, B_PIN)
 
 while 1:
-  delta = encoder.delta() # returns 0,1,or -1
+  delta = encoder.get_delta() # returns 0,1,or -1
   if delta!=0:
     print delta
