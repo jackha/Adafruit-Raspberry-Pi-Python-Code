d3a32b2a53e99485cd13d21723dac29a1692d401
Jack Ha <jackha@gmail.com>
2013-10-29 19:39:30 +0100

    a

--- test_rotary_display.py
+++ test_rotary_display.py
@@ -173,7 +173,7 @@ if __name__ == '__main__':
     server_thread.daemon = True
     server_thread.start()
 
-    sleep(1)
+    sleep(0.5)
     print "init to Pd..."
     send_sock.sendall('init;')  # makes Pd connect back on port 3001
 
