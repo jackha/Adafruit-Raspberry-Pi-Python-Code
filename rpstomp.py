dd84444ecc1f586bc1f475376512509bb1808959
Jack Ha <jackha@gmail.com>
2013-10-30 22:56:43 +0100

    added push buttons

--- rpstomp.py
+++ rpstomp.py
@@ -19,11 +19,13 @@ signal.signal(signal.SIGINT, signal_handler)
 
 ENC1_PIN_A = 5  # use wiring pin numbers here
 ENC1_PIN_B = 4
-ENC1_PIN_C = 6  # Push button
+#ENC1_PIN_C = 6  # Push button
 
 ENC2_PIN_A = 0  # use wiring pin numbers here
 ENC2_PIN_B = 2
-ENC2_PIN_C = 3  # Push button
+#ENC2_PIN_C = 3  # Push button
+
+PUSH_BUTTON_PINS = [6, 3, 12, 13, 14]
 
 EIGHT_BY_EIGHT_ADDRESS = 0x70
 EIGHT_BY_EIGHT_BRIGHTNESS = 0
@@ -138,6 +140,18 @@ class ListenThread(threading.Thread):
         print "Stopped listening to Pd."
 
 
+class PushButtons(object):
+    def __init__(self, button_pins, sleep_time=0.05):
+        self.button_pins = button_pins
+
+        for button_pin in self.button_pins:
+            self.gpio.pinMode(button_pin, self.gpio.INPUT)
+            self.gpio.pullUpDnControl(button_pin, self.gpio.PUD_UP)
+
+    def get_button(self, index):
+        return self.gpio.digitalRead(self.button_pins[index]) == wiringpi2.GPIO.LOW
+
+
 if __name__ == '__main__':
     print "Starting Raspberry-Stomp..."
 
@@ -154,12 +168,14 @@ if __name__ == '__main__':
 
     #encoder = rotary_encoder.RotaryEncoder(A_PIN, B_PIN)
     encoder1 = rotary_encoder.RotaryEncoder.Worker(
-        ENC1_PIN_A, ENC1_PIN_B, push_pin=ENC1_PIN_C, sleep_time=SLEEP_TIME)
+        ENC1_PIN_A, ENC1_PIN_B, sleep_time=SLEEP_TIME)
     encoder1.start()
 
     encoder2 = rotary_encoder.RotaryEncoder.Worker(
-        ENC2_PIN_A, ENC2_PIN_B, push_pin=ENC2_PIN_C, sleep_time=SLEEP_TIME)
+        ENC2_PIN_A, ENC2_PIN_B, sleep_time=SLEEP_TIME)
     encoder2.start()
+
+    push_buttons = PushButtons(PUSH_BUTTON_PINS)
 
     # Create a socket (SOCK_STREAM means a TCP socket)
     # client of puredata: use 'netreceive 3000' in pd
@@ -184,33 +200,52 @@ if __name__ == '__main__':
     option_names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
     #initialized = False
+    grid_needs_updating = False
+    push = {}
 
     while(True):
         # read rotary encoder
         delta1 = encoder1.get_delta()
         delta2 = encoder2.get_delta()
-        push1 = encoder1.get_button()
-        push2 = encoder2.get_button()
 
-        if delta1 != 0 or delta2 != 0 or push1 or push2 or startup:
+        #some_push = False
+        for i in range(len(PUSH_BUTTON_PINS)):
+            push[i] = push_buttons.get_button(i)
+            #if push[i]:
+            #    some_push = True
+
+        if push[0]:
+            push_timer_expiration = datetime.datetime.now() + datetime.timedelta(seconds=2)
+            grid.grid_array(janita)
+
+        if push[1]:
+            push_timer_expiration = datetime.datetime.now() + datetime.timedelta(seconds=2)
+            grid.grid_array(janita2)
+
+        if push[2] or push[3] or push[4]:
+            push_timer_expiration = datetime.datetime.now() + datetime.timedelta(seconds=2)
+            grid.grid_array(smiley)
+
+
+        if delta1 != 0 or delta2 != 0 or startup:
             selected = (selected + delta2) % (8*8)  # make it slower
             selected_idx = selected/8
             values[selected_idx] += delta1
             value = values[selected_idx]
-            print 'change value: selected %s(%s) value %s delta1 %d delta2 %d p1 %r p2 %r' % (
-                selected_idx, selected, value, delta1, delta2, push1, push2) 
+            print 'change value: selected %s(%s) value %s delta1 %d delta2 %d' % (
+                selected_idx, selected, value, delta1, delta2) 
             
-            if push1 or push2:
-                push_timer_expiration = datetime.datetime.now() + datetime.timedelta(seconds=4)
+            #if push1 or push2:
+            #    push_timer_expiration = datetime.datetime.now() + datetime.timedelta(seconds=4)
 
             #if push1 and not initialized:
             #    print "init to Pd..."
             #    send_sock.sendall('init;')  # makes Pd connect back on port 3001
             #    initialized = True
 
-            if push2:
-                print 'sending la la...'
-                send_sock.sendall('la la;')
+            #if push2:
+            #    print 'sending la la...'
+            #    send_sock.sendall('la la;')
 
             # Set 7 segment
             # Set hours
@@ -230,23 +265,12 @@ if __name__ == '__main__':
             send_sock.sendall('%s %d;' % (option_names[selected_idx], values[selected_idx]))
 
             startup = False
+            grid_needs_updating = True
 
-        # grid display
-        if push_timer_expiration > datetime.datetime.now():
-            # display smiley
-            #if push1:
-            #    grid.grid_array(smiley)
-            #if push2:
-            if push_timer_expiration - datetime.timedelta(seconds=2) > datetime.datetime.now() :
-                grid.grid_array(janita)
-            else:
-                grid.grid_array(janita2)
-        else:
-            # if value < 20:
-            #     grid.grid_array(smiley_neutral)
-            # elif value < 60:
-            #     grid.grid_array(smiley)
-            # else:
+        # grid display: default view
+        if datetime.datetime.now() > push_timer_expiration and grid_needs_updating or grid_needs_updating:
             grid.set_values(values, selected=selected_idx)
+            grid_needs_updating = False
+            push_timer_expiration = datetime.datetime.now()
 
         sleep(SLEEP_TIME)
