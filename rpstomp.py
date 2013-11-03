db2f36769633d82cfce56b95188ee7e535581664
Jack Ha <jackha@gmail.com>
2013-11-03 20:35:53 +0100

    a

--- rpstomp.py
+++ rpstomp.py
@@ -490,7 +490,11 @@ if __name__ == '__main__':
 
 
         if delta1 != 0 or delta2 != 0:
-            selected = (selected + delta2) % (len(effects.settings)*8)  # make it slower
+            selected = selected + delta2
+            if selected > len(effects.settings)*8: # *8 make it slower
+                selected = len(effects.settings)*8  
+            if selected < 0:
+                selected = 0
             selected_idx = selected/8
 
             value = effects.setting(selected_idx, delta1)
