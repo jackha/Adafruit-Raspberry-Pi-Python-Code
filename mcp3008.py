d42e20be0d24f0d36c743e8245e190903048eddf
Jack Ha <jackha@gmail.com>
2013-11-05 19:34:06 +0100

    a

---
+++ mcp3008.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import time
+import os
+import wiringpi2
+
+class Mcp3008(object):
+    def __init__(self, clockpin, misopin, mosipin, cspin):
+        self.gpio = wiringpi2.GPIO(wiringpi2.GPIO.WPI_MODE_PINS)
+        self.gpio.pinMode(clockpin, self.gpio.OUTPUT)
+        self.gpio.pinMode(misopin, self.gpio.INPUT)
+        self.gpio.pinMode(mosipin, self.gpio.OUTPUT)
+        self.gpio.pinMode(cspin, self.gpio.OUTPUT)
+        self.clockpin = clockpin
+        self.misopin = misopin
+        self.mosipin = mosipin
+        self.cspin = cspin
+
+    def read(self, adcnum):
+        # read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+        if ((adcnum > 7) or (adcnum < 0)):
+            return -1
+        self.gpio.output(self.cspin, True)
+        self.gpio.output(self.clockpin, False)
+        self.gpio.output(self.cspin, False)
+
+        commandout = adcnum
+        commandout |= 0x18  # start bit + single-ended bit
+        commandout <<= 3    # we only need to send 5 bits here
+
+        for i in range(5):
+            if (commandout & 0x80):
+                self.gpio.output(self.mosipin, True)
+            else:
+                self.gpio.output(self.mosipin, False)
+            commandout <<= 1
+            self.gpio.output(self.clockpin, True)
+            self.gpio.output(self.clockpin, False)
+
+        adcout = 0
+        # read in one empty bit, one null bit and 10 ADC bits
+        for i in range(12):
+            self.gpio.output(self.clockpin, True)
+            self.gpio.output(self.clockpin, False)
+            adcout <<= 1
+            if (self.gpio.input(self.misopin)):
+                adcout |= 0x1
+
+        self.gpio.output(self.cspin, True)
+        
+        adcout >>= 1       # first bit is 'null' so drop it
+        return adcout
+
+if __name__ == '__main__':
+    # change these as desired - they're the pins connected from the
+    # SPI port on the ADC to the Cobbler
+    # SPICLK = 24
+    # SPIMISO = 25
+    # SPIMOSI = 8
+    # SPICS = 7
+
+    SPICLK = 5
+    SPIMISO = 6
+    SPIMOSI = 10
+    SPICS = 11
+
+    mcp = Mcp3008(SPICLK, SPIMISO, SPIMOSI, SPICS)
+
+    while True:
+        print mcp.read(0)
+
+        time.sleep(0.01)
