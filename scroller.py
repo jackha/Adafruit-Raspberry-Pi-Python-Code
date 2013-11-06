abe87a65f3b25cb6c30e9f5a4c8ca6941d72e462
Jack Ha <jackha@gmail.com>
2013-11-06 21:35:53 +0100

    a

--- scroller.py
+++ scroller.py
@@ -19,7 +19,6 @@ class Scroller(object):
         self.max_pos = len(self.scroll_array[0])
         self.pos = 0
         self.byte_array = self.char_height * [0]  # for sending to display
-        print self.scroll_array
 
     def up(self):
         """move 1 pixel"""
