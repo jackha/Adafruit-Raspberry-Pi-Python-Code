cd3707ba9e3d0c223c226ae3b9fc3ddf4a2de255
Jack Ha <jackha@gmail.com>
2013-11-09 15:18:36 +0100

    a

--- bye.py
+++ bye.py
@@ -4,10 +4,8 @@ from rpstomp import SevenSegmentPlus
 from rpstomp import EightByEightPlus
 import smiley
 
-grid = EightByEightPlus(address=0x70)
-segment = SevenSegmentPlus(address=0x74)
+grid = EightByEightPlus(address=0x70, brightness=0)
+segment = SevenSegmentPlus(address=0x74, brightness=0)
 
-segment.write(' bye')
-segment.setColon(1)              # Toggle colon at 1Hz
-
+segment.write('byee')
 grid.grid_array(smiley.smiley_cry)
