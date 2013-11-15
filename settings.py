0843bca27dce4c873ed49e99991f2a09c63699f4
Jack Ha <jackha@gmail.com>
2013-11-15 14:19:51 +0100

    a

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ off = [
 
 stutter = [
 	{'name': ' dly', 'min': 10, 'max': 1000, 'default': 200, 'type': 'float', 'exp1': {'min': 10, 'max': 1000}},
-	{'name': 'amnt', 'min': 0, 'max': 1, 'default': 0.3, 'type': 'float'},
+	{'name': 'amnt', 'min': 0, 'max': 1, 'default': 0.6, 'type': 'float'},
 ]
 
 spectraldelay = [
