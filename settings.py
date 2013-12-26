51fd43c40c20b5fc6da969aada4fd89f39a0438c
Jack Ha <jackha@gmail.com>
2013-12-26 13:05:20 +0100

    a

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ midi_osc = [
 	{'name': ' saw', 'min': 0, 'max': 1, 'default': 1, 'type': 'float', 'display': MIX_DISPLAY},
 	{'name': 'squa', 'min': 0, 'max': 1, 'default': 0, 'type': 'float', 'display': MIX_DISPLAY},
 	{'name': 'tune', 'min': -4, 'max': 4, 'default': 0., 'type': 'integer'},
-	{'name': 'ftun', 'min': -7, 'max': 7, 'default': 0., 'type': 'float', 'exp1': {'min': 0.5, 'max': 0.5}},
+	{'name': 'ftun', 'min': -7, 'max': 7, 'default': 0., 'type': 'float', 'exp1': {'min': -7, 'max': 7}},
 	{'name': 'ftu2', 'min': -3.5, 'max': 3.5, 'default': 0., 'type': 'float'},
 	]
 
